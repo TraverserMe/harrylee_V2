b2bddc8409302c07ed3f451d2213760a1b9f7b9c
TraverserMe <leekinnangharry@gmail.com>
2024-07-16 00:02:17 +0800

    start up

---
+++ components/navbar.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { ModeToggle } from "@/components/mode-toggle";
+import { cn } from "@/lib/utils";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import * as React from "react";
+
+const components: { title: string; href: string; description: string }[] = [
+    {
+        title: "Background",
+        href: "/",
+        description: "It's a great place to start my introduction of myself.",
+    },
+    {
+        title: "Projects",
+        href: "",
+        description:
+            "For sighted users to preview content available behind a link.",
+    },
+    {
+        title: "Work Experience",
+        href: "/",
+        description:
+            "The place to find out about my professional experience and my skills.",
+    },
+];
+
+const ListItem = React.forwardRef<
+    React.ElementRef<"a">,
+    React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+    return (
+        <li>
+            <NavigationMenuLink asChild>
+                <a
+                    ref={ref}
+                    className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        className
+                    )}
+                    {...props}
+                >
+                    <div className="text-sm font-medium leading-none">
+                        {title}
+                    </div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        {children}
+                    </p>
+                </a>
+            </NavigationMenuLink>
+        </li>
+    );
+});
+ListItem.displayName = "ListItem";
+
+export default function Navbar() {
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <Link href={"/"}>
+                        {" "}
+                        <Image
+                            src={"/logo.png"}
+                            alt="logo"
+                            width={50}
+                            height={50}
+                        />
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>
+                        Getting started
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                            <li className="row-span-3">
+                                <NavigationMenuLink asChild>
+                                    <Link
+                                        className="flex h-full w-full select-none flex-col justify-end bg-slate-100 rounded-md bg-gradient-to-b 
+                                        from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                        href="/"
+                                    >
+                                        <Image
+                                            src={"/harry.svg"}
+                                            alt="logo"
+                                            width={200}
+                                            height={200}
+                                        />
+                                        <div className="mb-2 mt-4 text-lg font-medium">
+                                            My name is Harry
+                                        </div>
+                                        <p className="text-sm leading-tight text-muted-foreground">
+                                            I'm a software engineer. I want to
+                                            learn more about web development. I
+                                            hope that I could be a great
+                                            developer in the future.
+                                        </p>
+                                    </Link>
+                                </NavigationMenuLink>
+                            </li>
+                            <ListItem href="/docs" title="Introduction">
+                                Re-usable components built using Radix UI and
+                                Tailwind CSS.
+                            </ListItem>
+                            <ListItem
+                                href="/docs/installation"
+                                title="Installation"
+                            >
+                                How to install dependencies and structure your
+                                app.
+                            </ListItem>
+                            <ListItem
+                                href="/docs/primitives/typography"
+                                title="Typography"
+                            >
+                                Styles for headings, paragraphs, lists...etc
+                            </ListItem>
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>About Me</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                            {components.map((component) => (
+                                <ListItem
+                                    key={component.title}
+                                    title={component.title}
+                                    href={component.href}
+                                >
+                                    {component.description}
+                                </ListItem>
+                            ))}
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <ModeToggle />
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    );
+}
