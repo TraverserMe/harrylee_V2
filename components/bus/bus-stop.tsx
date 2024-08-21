07549b247b732ffaebdf44ead4ca7cbb77cccabc
TraverserMe <leekinnangharry@gmail.com>
2024-08-21 16:37:38 +0800

    try on client side

--- components/bus/bus-stop.tsx
+++ components/bus/bus-stop.tsx
@@ -1,10 +1,91 @@
 "use client";
 
-import { getNearByBusStops } from "@/action/bus";
+import { getAllBusStops, getNearByBusStops } from "@/action/bus";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { StopETA, StopInfo } from "@/schemas/bus";
-import { use, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import BusRow from "@/components/bus/bus-page-row";
+import { calculateDistance } from "@/utils/bus";
+
+async function getData({
+    userLocation,
+    range,
+}: {
+    userLocation: { lat: number; long: number };
+    range: number;
+}) {
+    const allBusStops = await getAllBusStops();
+    var nearByBusStops = [] as StopInfo[];
+    //fetch the nearby bus stops ETA
+    var nearestBusStopETA = [] as StopETA[];
+
+    //calculate distance between user and bus stops in range
+    for (let i = 0; i < allBusStops.length; i++) {
+        const d = calculateDistance(
+            userLocation.lat,
+            userLocation.long,
+            allBusStops[i].lat,
+            allBusStops[i].long
+        );
+        if (d > 500) {
+            continue;
+        }
+        if (d <= range) {
+            nearByBusStops.push(allBusStops[i]);
+        }
+    }
+
+    if (!nearByBusStops.length) {
+        nearByBusStops = allBusStops.slice(0, 30);
+    }
+
+    for (let i = 0; i < nearByBusStops.length; i++) {
+        const res = await fetch(
+            "https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/" +
+                nearByBusStops[i].stop,
+            {
+                cache: "no-cache",
+            }
+        );
+        const json = await res.json();
+
+        const busStopETA = json.data as {
+            co: "KMB";
+            route: string;
+            dir: string;
+            service_type: string;
+            seq: number;
+            dest_tc: string;
+            dest_en: string;
+            eta_seq: number;
+            eta: string;
+            rmk_tc: string;
+            rmk_en: string;
+        }[];
+        // just take the first one of each route
+
+        busStopETA.map((stopETA) => {
+            if (
+                stopETA.eta_seq === 1 &&
+                //not already in the list and not the same direction
+                nearestBusStopETA.findIndex(
+                    (x) => x.route === stopETA.route && x.dir === stopETA.dir
+                ) === -1 &&
+                stopETA.eta
+            ) {
+                nearestBusStopETA.push({
+                    stop: nearByBusStops[i].name_tc,
+                    ...stopETA,
+                });
+            }
+        });
+    }
+
+    return {
+        busStops: nearByBusStops,
+        busStopETA: nearestBusStopETA,
+    };
+}
 
 function BusStop() {
     const [userLocation, setUserLocation] = useState({
@@ -24,7 +105,7 @@ function BusStop() {
             return;
         }
         navigator.geolocation.getCurrentPosition((position) => {
-            getNearByBusStops({
+            getData({
                 userLocation: {
                     lat: position.coords.latitude,
                     long: position.coords.longitude,
